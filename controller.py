431c174348af9d4d0ab08f1a0feddb99c3b2dc26
Svyatoslav Tsar <79832144+Svts3@users.noreply.github.com>
2022-02-08 11:16:06 +0200

    Added 3 files

---
+++ controller.py
@@ -0,0 +1,16 @@
+
+from main import app
+@app.route('/')
+def index():
+    return "Hooray"
+@app.route("/fish/<id>")
+def get_fish():
+    print(id)
+    return "My fish is gourgeus"
+@app.route("/fish")
+def get_all_fishes():
+    return "All fishes"
+
+@app.route("/fish", methods = ["POST"])
+def create_fish():
+    return "fish was created"
